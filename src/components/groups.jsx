1ddb8fae6eb42d5f51531a15cab543195f4340b3
Desamsetti <desamses@ES.AD.ADP.com>
2023-08-31 23:27:33 +0530

    updated group

--- src/components/groups.jsx
+++ src/components/groups.jsx
@@ -1,4 +1,5 @@
 import React,{useEffect, useState} from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const Groups = () => {
@@ -10,8 +11,40 @@ const Groups = () => {
     //console.log(isAuth)
   }, [])
 
+  const [grpname, setGrapName] = useState()
+
+
+  const SubmitGrp = async () => {
+    const token = localStorage.getItem('access_token')
+    console.log(token)
+    console.log(grpname)
+    await axios.post('http://127.0.0.1:8000/formgroup/', {
+      grpname : grpname
+    }, {
+      headers: {
+          Authorization: `Bearer ${token}`
+      },      
+  })
+  }
+
+
   return (
+    <>
     <div>Groups</div>
+    <div className="add_group_div">
+      <div className="add_group_btn">
+        Add group 
+      </div>
+    </div>
+    <div className="add_group_div">
+      <div className="add_gorup_form">
+        <div>
+          <input type="text" name='group name' onChange={(e)=>setGrapName(e.target.value)} />
+          <button onClick={SubmitGrp}>Create Group</button>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
