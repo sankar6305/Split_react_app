8105ffdf274c53b660b407e4c11e0923bedfa248
sankar6305 <desamsettisankar@gmail.com>
2023-09-04 20:53:09 +0530

    adding images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,26 +12,30 @@ const Header = () => {
                 SplitUp
             </div>
         <div className="Header-main-nav-components">
-            <div className="Header-main-component" onClick={(e) => navigate('/')}>
-                Home
-            </div>
             <div className="Header-main-component" onClick={(e) => navigate('/groups')}>
+                <div className="groups_img_div"> <img src={require("./Images/groups_image.png")} alt="Group_icon" /> </div>
                 Groups
             </div>
             <div className="Header-main-component" onClick={(e) => navigate('/activity')}>
+            <div className="groups_img_div"> <img src={require("./Images/activity.png")} alt="Group_icon" /> </div>
                 Activity
             </div>
             <div className="Header-main-component" onClick={(e) => navigate('/settings')}>
+            <div className="groups_img_div"> <img src={require("./Images/settings.png")} alt="Group_icon" /> </div>
                 Settings 
             </div>
         </div>
         <div className="login_logout_buttons">
+        <div className="groups_img_div"> <img src={require("./Images/login.png")} alt="Group_icon" /> </div>
             <div className="login" onClick={(e)=>navigate('/login')} >
+            
                 Login
             </div>
         </div>
         <div className="login_logout_buttons">
+        <div className="groups_img_div"> <img src={require("./Images/regester.png")} alt="Group_icon" /> </div>
             <div className="login" onClick={(e)=>navigate('/register')} >
+            
                 Register
             </div>
         </div>
