a42d0088d96cd7043512fff136b467e7050a8978
sankar6305 <desamsettisankar@gmail.com>
2023-09-17 22:09:08 +0530

    added forgot password

--- src/components/settingsComponent/ChangePassword.jsx
+++ src/components/settingsComponent/ChangePassword.jsx
@@ -5,9 +5,19 @@ const ChangePassword = () => {
   const [emailid, setEmailID] = useState(localStorage.getItem('email'))
   const [passwordchanged, setPassowordChanged] = useState('')
 
-  const HandlePasswordChange = () => {
+  const HandlePasswordChange = async() => {
     console.log(emailid);
     console.log(passwordchanged);
+    const token = localStorage.getItem('access_token')
+
+    await axios.post("http://127.0.0.1:8000/changepassword/", {
+        email : emailid,
+        password: passwordchanged
+    }, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        },      
+    }).then((data)=>console.log(data))
   }
 
   return (
@@ -16,8 +26,8 @@ const ChangePassword = () => {
         <div className='JustCardPart'>
           <div className='PasswordChange'>
             <div className='MakeGap'>Change Password </div>
-            <div className='MakeGap'><input type="text" value={emailid} onChange={(e)=>setEmailID(e.target.value)} placeholder='enter email address' /></div>
-            <div className='MakeGap'><input type="text" value={passwordchanged} onChange={(e)=>setPassowordChanged(e.target.value)} placeholder='enter the password to reset' /></div>
+            <div className='MakeGap'><input type="text" value={emailid} placeholder='enter email address' /></div>
+            <div className='MakeGap'><input type="text" value={passwordchanged} onChange={(e)=>setPassowordChanged(e.target.value)} placeholder='enter the password' /></div>
             <div className='MakeGap'><button onClick={HandlePasswordChange}>Submit</button></div>
           </div>
         </div>
